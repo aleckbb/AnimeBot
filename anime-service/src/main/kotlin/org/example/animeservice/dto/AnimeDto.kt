326f60dd6db66750283b83c53a92e8f29147cb6e
electroem3000 <em_vera@mail.ru>
2025-06-15 18:08:02 +0300

    added AnimeController and animeService

---
+++ anime-service/src/main/kotlin/org/example/animeservice/dto/AnimeDto.kt
@@ -0,0 +1,9 @@
+package org.example.animeservice.dto
+
+
+class AnimeDto(
+    val title: String,
+    val id: String? = null,
+) {
+
+}
--- telegram-bot/build.gradle.kts
+++ telegram-bot/build.gradle.kts
@@ -34,6 +34,11 @@ dependencies {
     // Jackson
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.1")
 
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    // (необязательно) если планируете писать корутинный WebClient:
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
 }
 
 kotlin {
