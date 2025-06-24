1d978d572c8d713b74b0cc80ceee2fd853425e90
electroem3000 <em_vera@mail.ru>
2025-06-24 21:59:29 +0300

    corrected formating

--- telegram-bot/src/test/kotlin/io/proj3ct/telegrambot/utils/TestData.kt
+++ telegram-bot/src/test/kotlin/io/proj3ct/telegrambot/utils/TestData.kt
@@ -1,15 +1,8 @@
 package io.proj3ct.telegrambot.utils
 
-import java.time.Instant
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.ZoneId
-
 object TestData {
-
     const val CHAT_ID = 1L
     const val MESSAGE_ID = 1
 
     val TITLE = "Стальной алхимик: Братство"
-
 }
